3d5052dfaa4f76839a8da98ade92cd59c9c36aa1
Eugeniu <98963081+VATARRRI@users.noreply.github.com>
2022-06-30 20:52:14 +0300

    Update Configs.java

--- src/main/java/com/example/registrationappwithspring/Configs.java
+++ src/main/java/com/example/registrationappwithspring/Configs.java
@@ -4,6 +4,6 @@ public class Configs {
     protected String dbHost = "127.0.0.1";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "fidesco21";
+    protected String dbPass = "sql123123";
     protected String dbName = "database";
 }
